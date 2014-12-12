7f38f0a37b32e2466b365ae78cddd881bcd0b89b
utkpython <utkpython@gmail.com>
2014-12-12 14:31:38 -0500

    Update automate_jmp_tasks.r

--- automate_jmp_tasks.r
+++ automate_jmp_tasks.r
@@ -1,1 +1,14 @@
 # some quick scripts to replicate some JMP functionality (minus the GUI parts...)
+
+# snag a basic csv file
+# attach it
+
+# similar to "analyze distribution" 
+# get a histogram for a column
+# get the summary statistics 
+# test for normality: Wald test?
+
+
+# do a t test on a column
+
+# 
