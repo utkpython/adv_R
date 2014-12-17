796627c17927d70222acd8d4789d3b8a119c997e
utkpython <utkpython@gmail.com>
2014-12-17 13:23:21 -0500

    Update automate_jmp_tasks.r

--- automate_jmp_tasks.r
+++ automate_jmp_tasks.r
@@ -8,9 +8,12 @@ attach(myData)
 # similar to "analyze distribution" 
 # get a histogram for a column
 hist(myColumn,main="your title")
-legend("topright",paste("Mean = ", round(mean(myColumn),1),
-       "\nMed. = ",round(median(myColumn),1),
-       "\nSD = ", round(sd(myColumn),1)),bty="n")
+legend("topright",
+       paste("Mean = ", round(mean(myColumn),1),
+             "\nMed. = ",round(median(myColumn),1),
+             "\nSD = ", round(sd(myColumn),1)),
+       bty="n")
+       
 # get the summary statistics 
 summary(myColumn)
 # test for normality: Shapiro-Wilk test?
