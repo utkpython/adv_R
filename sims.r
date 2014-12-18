3ca3c5c7bc932d54d31527cfe5c8f0312f3536f2
utkpython <utkpython@gmail.com>
2014-12-18 10:53:03 -0500

    Update sims.r

--- sims.r
+++ sims.r
@@ -1,2 +1,6 @@
 # really really basic framework for agent based models
 # maybe we could do dating or something fun
+
+# there has been some research done about dating & game theory
+# can be modeled as an assignment problem
+
