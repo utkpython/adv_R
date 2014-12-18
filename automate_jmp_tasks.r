2206d5d64a84ff0cca1bfdfc163dd0b14de74243
utkpython <utkpython@gmail.com>
2014-12-18 11:48:00 -0500

    Update automate_jmp_tasks.r

--- automate_jmp_tasks.r
+++ automate_jmp_tasks.r
@@ -1,10 +1,12 @@
 # some quick scripts to replicate some JMP functionality (minus the GUI parts...)
 
+#---------------------------------
 # snag a basic csv file
 myData <- read.csv(path_to_your_data.csv,header=TRUE) # or FALSE, depending on the file
 # attach it
 attach(myData)
 
+#----------------------------------
 # similar to "analyze distribution" 
 # get a histogram for a column
 hist(myColumn,main="your title")
@@ -30,3 +32,10 @@ t.test(myColumn,mu=mu_theory)
 
 # printing out a summary for all the columns
 for (i in names(myData)) { print(c(i,summary(myData[[i]])))} #this is really sloppy looking, need better way to do it
+
+#-------------------------------
+# really basic model fitting
+
+# fit y by x
+
+my_model <- lm(yColumn~xColumn)
