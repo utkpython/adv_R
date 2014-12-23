865c6eb9d8d16f288d6c11edf22ce15f49ad360e
utkpython <utkpython@gmail.com>
2014-12-23 13:34:47 -0500

    Update ggplot.r

--- ggplot.r
+++ ggplot.r
@@ -1,3 +1,9 @@
 # hopefully we'll learn ggplot soon enough
 
 # we can provide some templates for the students
+
+
+# histograms
+# ---------------------------------------------
+qplot(myData$column)
+
