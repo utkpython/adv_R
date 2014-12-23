12f07fde40cf07ea1810c25f5b0d326ce04df116
utkpython <utkpython@gmail.com>
2014-12-23 11:21:02 -0500

    changed avg to mean

--- waittimesim.r
+++ waittimesim.r
@@ -4,13 +4,13 @@ srvtime    <- 0
 arvtime    <- 0
 arrival_tm <- rexp(1, 5)       #how long before customers arrive(with a rate of 5 an hour avg)
 time       <- time + arrival_tm
-waittimes  <- c()
+wait_times  <- c()
 serv       <- c()
 arv        <- c()
 
 
 while(arvtime < 8) {
-  servc_tm   <- rexp(1, 5)     #how long it takes to servie them
+  servc_tm   <- rexp(1, 5)     #how long it takes to serve them
   serv       <- c(serv, servc_tm)
   
   arrival_tm <- rexp(1, 5)     #arrival of the next person
@@ -19,13 +19,13 @@ while(arvtime < 8) {
   
   if (servc_tm+srvtime > arvtime) {
     srvtime   <- srvtime + servc_tm
-    waittimes <- c(waittimes, (srvtime-arvtime))
+    wait_times <- c(wait_times, (srvtime-arvtime))
   } else {
     srvtime   <- srvtime + arvtime
-    waittimes <- c(waittimes, 0)
+    wait_times <- c(wait_times, 0)
   }
 }
 
-waittimes
-avg(waittimes)
+wait_times
+mean(wait_times)
 
