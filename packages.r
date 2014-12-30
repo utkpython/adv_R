57044c263b15cffe89170e63a840c1b8a7e7ee48
utkpython <utkpython@gmail.com>
2014-12-30 13:42:54 -0500

    Update packages.r

--- packages.r
+++ packages.r
@@ -8,23 +8,28 @@
 # makes handling data easier
 
 install.packages("plyr")
+library(plyr)
+
 
 
 # -------------
 # ggplot2
 # -------------
-
 # makes plotting fun again
 # also a buzzword in data science cliques
 
 install.packages("ggplot2")
+library(ggplot2)
+
+
 
 # ------------
 # fitdistrplus
 # ------------
-
 # for all those distributions you have to fit
 
+install.packages("fitdistrplus")
+library(fitdistrplus)
 
 # ------------
 # devtools, specifically for install_github
